aab35e10b746a925bb4a009ebede77b104130079
Ali <90877977+AlhakeemALI@users.noreply.github.com>
2023-01-20 18:14:05 -0700

    final commit

--- script.js
+++ script.js
@@ -4,10 +4,48 @@ const left = document.querySelector(".left")
 const right = document.querySelector(".right")
 const slider = document.querySelector(".slider")
 const images = document.querySelectorAll(".image")
+const bottom = document.querySelector(".bottom")
 
 // creat 
 let sliderNumber = 1
 const length = images.length
+
+
+// creat circal under each image
+
+for (let i = 0; i< length; i++) {
+  const div  = document.createElement("div");
+  div.className = "button"
+  bottom.appendChild(div)
+}
+
+
+const buttons = document.querySelectorAll(".button")
+buttons[0].style.backgroundColor = "black"
+
+const resetBg = () => {
+  buttons.forEach((button) => {
+    button.style.backgroundColor = "transparent"
+  })
+}
+
+buttons.forEach((button, i) => {
+  button.addEventListener("click", () => {
+    resetBg()
+    slider.style.transform = `translateX(-${ i * 800}px)`
+    sliderNumber = i + 1
+    button.style.backgroundColor = "black"
+  })
+})
+
+
+// creat function to change color circal
+
+const changeColor = () => {
+  resetBg()
+  buttons[sliderNumber-1].style.backgroundColor = "black"
+}
+
 
 // add function to move the slide to  right
 
@@ -23,12 +61,27 @@ const getFirstSlide = () => {
 
 right.addEventListener("click", () => {
 sliderNumber < length ? nextSlide() :getFirstSlide()
+changeColor()
 
 })
 
 
 // add a function to move the slide to the left
 
+const getLastSlide = () => {
+  slider.style.transform = `translateX( - ${(length -1 * 800)}px )`
+  sliderNumber = length
+}
+
+const prevSlide = () => {
+  slider.style.transform = `translateX(-${(sliderNumber - 2) * 800}px)`
+  sliderNumber--;
+}
+
+left.addEventListener('click', () => {
+  sliderNumber > 1 ? prevSlide() : getLastSlide()
+  changeColor()
+})
 
 
 
